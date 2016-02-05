502ba36f0fa2d93d855b6120be66fbf174be96cc
ZhengTang1120 <zhengtan@usc.edu>
2016-02-04 23:36:21 -0800

    new setup

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from distutils.core import setup
 from setuptools import Extension,find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'faerie',
-  version = '0.0.2',
+  version = '0.1.0',
   description = 'Dictionary-based entity extraction with efficient filtering',
+  long_description=long_description,
   author = 'Zheng Tang',
   author_email = 'zhengtan@isi.edu',
   url = 'https://github.com/ZhengTang1120/entity_extraction/', # use the URL to the github repo
@@ -11,5 +22,6 @@ setup(
   keywords = ['heap', 'entity_extraction'], # arbitrary keywords
   ext_modules=[Extension(
         'singleheap',
-        ['singleheap.c'])]
+        ['singleheap.c'])],
+  install_requires=['nltk']
 )
